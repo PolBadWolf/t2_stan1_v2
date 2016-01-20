b34511a0e3857a665ab1080adfe703dbf1dc8797
Marat Gumerov <gm.gameoff@gmail.com>
2016-01-20 13:14:41 +0500

    try bufferRecive[] fix check Parameters

--- test2/Writer.cs
+++ test2/Writer.cs
@@ -78,171 +78,6 @@ namespace test2
                     Console.WriteLine(ex.ToString());
                 }
                 write_newTube = null;
-                /*
-                int hasDeffect = 0;
-                mainWindow.Dispatcher.Invoke(new ThreadStart(delegate
-                {
-                    try { connection.Open(); }
-                    catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("Open() for \"INSERT INTO defectsdata\"");
-                    }
-                    //              defectsdata
-                    MySqlCommand myCommand = new MySqlCommand("INSERT INTO defectsdata(defectsdata.NumberPart, defectsdata.NumberTube, defectsdata.NumberSegments, defectsdata.DataSensors, defectsdata.DatePr, defectsdata.TimePr, defectsdata.Porog, defectsdata.Current, defectsdata.FlDefectTube) values(@A, @B, @C, @D, @E, @F, @G, @H, @I)", connection.mySqlConnection);
-                    // номер партии
-                    myCommand.Parameters.AddWithValue("A", mainWindow.Parameters["part"]);
-                    // порог
-                    myCommand.Parameters.AddWithValue("G", mainWindow.Parameters["porog"]);
-                    // ток
-                    myCommand.Parameters.AddWithValue("H", mainWindow.Parameters["current"]);
-                    // номер трубы
-                    myCommand.Parameters.AddWithValue("B", LastNumberTube(mainWindow.Parameters["part"]) + 1);
-                    // размер трубы
-                    myCommand.Parameters.AddWithValue("C", buffForRead[5]);
-                    // дефекты
-                    Byte[] deffectsArray = new Byte[buffForRead[5]];
-                    for (int k = 0; k < (int)buffForRead[5]; k++)
-                    {
-                        if (bufferRecive[k] != 0) hasDeffect = 1;
-                        deffectsArray[k] = bufferRecive[k];
-                    }
-                    myCommand.Parameters.AddWithValue("D", deffectsArray);
-                    // текущая дата
-                    DateTime theDateTime = DateTime.Now;
-                    myCommand.Parameters.AddWithValue("E", theDateTime.ToString("yyyy-MM-dd"));
-                    myCommand.Parameters.AddWithValue("F", theDateTime.ToString("H:mm:ss"));
-                    // наличие дефектов
-                    myCommand.Parameters.AddWithValue("I", hasDeffect);
-                    try { myCommand.ExecuteNonQuery(); } catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("\"INSERT INTO defectsdata\"");
-                    }
-                    //==============
-                    try { connection.Close(); } catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("\"INSERT INTO defectsdata\" - close()");
-                    }
-                    try { connection.Open(); } catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("Open() for Read last IndexData ");
-                    }
-                    // чтение индекса
-                    myCommand = new MySqlCommand(@"
-SELECT
-IndexData
-FROM defectsdata
-ORDER BY IndexData DESC
-LIMIT 1
-", connection.mySqlConnection);
-                    Int64 lastIndex = 0;
-                    MySqlDataReader dataRead = null;
-                    try
-                    {
-                        dataRead = myCommand.ExecuteReader();
-                        dataRead.Read();
-                        lastIndex = dataRead.GetInt64(0);
-                    }
-                    catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("Read last IndexData");
-                    }
-                    try
-                    {
-                        dataRead.Close();
-                        connection.Close();
-                    }
-                    catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("Close() for Read last IndexData ");
-                    }
-                    var ko = mainWindow.Parameters["control_sample"];
-                    //              indexes
-                    try { connection.Open(); }
-                    catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("Open() for \"INSERT INTO indexes\"");
-                    }
-                    myCommand = new MySqlCommand(@"
-INSERT INTO 
-indexes (
-Version
-,IndexData
-,Id_SizeTube
-,Id_Gost
-,Id_ControlSample
-,Id_WorkSmen
-,Id_TimeIntervalSmen
-,Id_Operator1
-,Id_Operator2
-,Id_Device
-,Id_Sensor
-,Id_NameDefect
-) VALUES (
-@Vers
-,@Id_Index
-,@Id_SizeTube
-,@Id_Gost
-,@Id_ControlSamle
-,@Id_WorkSmen
-,@Id_TimeIntervalSmen
-,@Id_Operator1
-,@Id_Operator2
-,@Id_Device
-,@Id_Sensor
-,@Id_NameDefect
-)", connection.mySqlConnection);
-                    myCommand.Parameters.Clear();
-                    myCommand.Parameters.AddWithValue("Vers", 2);
-                    myCommand.Parameters.AddWithValue("Id_Index", lastIndex);
-                    myCommand.Parameters.AddWithValue("Id_SizeTube", mainWindow.Parameters["diameter"]);
-                    myCommand.Parameters.AddWithValue("Id_Gost", mainWindow.Parameters["gost"]);
-                    myCommand.Parameters.AddWithValue("Id_ControlSamle", (int)mainWindow.Parameters["control_sample"]);
-                    myCommand.Parameters.AddWithValue("Id_WorkSmen", mainWindow.Parameters["smena"]);
-                    myCommand.Parameters.AddWithValue("Id_TimeIntervalSmen", mainWindow.Parameters["smena_time"]);
-                    myCommand.Parameters.AddWithValue("Id_Operator1", mainWindow.Parameters["operator1"]);
-                    myCommand.Parameters.AddWithValue("Id_Operator2", mainWindow.Parameters["operator2"]);
-                    myCommand.Parameters.AddWithValue("Id_Device", mainWindow.Parameters["device"]);
-                    myCommand.Parameters.AddWithValue("Id_Sensor", mainWindow.Parameters2["ho"]);
-                    myCommand.Parameters.AddWithValue("Id_NameDefect", mainWindow.Parameters["name_defect"]);
-                    try
-                    { myCommand.ExecuteNonQuery(); }
-                    catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("\"INSERT INTO indexes\"");
-                    }
-                    try { connection.Close(); } catch
-                    {
-                        Console.WriteLine("========================================");
-                        Console.WriteLine("Write.cs");
-                        Console.WriteLine("Recived_NewTube()  :  " + DateTime.Now.ToString());
-                        Console.WriteLine("indexes close()");
-                    }
-                }));
-                */
             }
             else
             {
@@ -272,12 +107,18 @@ Version
             }));
             mainWindow.Move_Tube();
             // запись дефектов
-            bufferRecive[buffForRead[5]] = buffForRead[6];
+            try{ bufferRecive[buffForRead[5]] = buffForRead[6]; } catch (Exception ex)
+            {
+                Console.WriteLine("========================================");
+                Console.WriteLine("Write.cs");
+                Console.WriteLine("Recived_SegmentTube()  :  " + DateTime.Now.ToString());
+                Console.WriteLine("Error index : " + buffForRead[5].ToString());
+                Console.WriteLine(ex.ToString());
+            }
             // отображение дефектов
             if (buffForRead[6]!=0)
             {
                 mainWindow.Error_Segment();
-//                bufferRecive[]
             }
         }
         private void Recived_Sample()
@@ -298,7 +139,6 @@ Version
                     }
                     sampleDataCount++;
                 }
-                //if (sampleDataCount >= 39)
                 else
                 {
                     mainWindow.ButtonCancel.IsEnabled = true;
@@ -318,7 +158,7 @@ Version
         public void SaveSample()
         {
             Connection connection = new Connection();
-            if (mainWindow.Parameters.Count == 13)
+            if ((mainWindow.Parameters.Count == 12) && (mainWindow.Parameters2.Count == 1))
             {
                 int hasDeffect = 0;
                 //                defectsdata
@@ -469,75 +309,6 @@ VALUES
         public void PortClose()
         {
             serialPort.Close();
-        }
-        //==
-        private int LastNumberTube(int part)
-        {
-            Connection connection = new Connection();
-            int last = 0;
-            try
-            {
-                connection.Open();
-            }
-            catch
-            {
-                Console.WriteLine("========================================");
-                Console.WriteLine("Write.cs");
-                Console.WriteLine("LastNumberTube()");
-                Console.WriteLine("Open for ExecuteReader");
-            }
-            MySqlCommand myCommand = new MySqlCommand(@"
-SELECT
-NumberTube, NumberPart
-FROM defectsdata
-WHERE NumberTube<>0
-ORDER BY IndexData DESC
-LIMIT 1", connection.mySqlConnection);
-            MySqlDataReader mySqlReader = null; ;
-            try
-            {
-                mySqlReader = myCommand.ExecuteReader();
-            }
-            catch
-            {
-                Console.WriteLine("========================================");
-                Console.WriteLine("Write.cs");
-                Console.WriteLine("LastNumberTube()");
-                Console.WriteLine("ExecuteReader");
-            }
-            while (mySqlReader.Read())
-            {
-                try
-                {
-                    if ((mySqlReader.GetValue(mySqlReader.GetOrdinal("NumberTube")) == null) || (mySqlReader.GetInt32(mySqlReader.GetOrdinal("NumberPart")) != part))
-                        last = 0;
-                    else
-                        last = mySqlReader.GetInt32(mySqlReader.GetOrdinal("NumberTube"));
-                }
-                catch
-                {
-                    Console.WriteLine("========================================");
-                    Console.WriteLine("Write.cs");
-                    Console.WriteLine("LastNumberTube()");
-                    Console.WriteLine("ExecuteReader - READ NumberTube, NumberPart, NumberTube");
-                }
-            }
-            mySqlReader.Close();
-            /*
-            //out
-            mainWindow.Dispatcher.Invoke(new ThreadStart(delegate
-            {
-                mainWindow.lblinfo6.Content = "Пройдено труб:\t\t " + last;
-            }));
-            */
-            try { connection.Close(); } catch
-            {
-                Console.WriteLine("========================================");
-                Console.WriteLine("Write.cs");
-                Console.WriteLine("LastNumberTube()");
-                Console.WriteLine("Close for ExecuteReader");
-            }
-            return last;
         }
     }
 }
